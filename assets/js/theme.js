bb092a585ed4a06582a62bdb8749c3dfab6de19c
Aradea Atfal Risdianto <aradeaa9@gmail.com>
2020-07-23 17:05:40 +0700

    Change

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,18 +1,12 @@
 $(document).ready(function () {
-  $("#profile").hide();
-  $(".name").click(function () {
-    $(".name").fadeOut(1200);
-    $(".profession").fadeOut(1200);
-    $(".arrow-left").fadeOut(1200);
-    $(".arrow-right").fadeOut(1200);
-    $("#profile").delay(1200).fadeIn(1200);
+  $(".introduce-details").hide();
+  $("#aboutBtn").click(function () {
+    $(".introduce").fadeOut(1200);
+    $(".introduce-details").delay(1200).fadeIn(1200);
   });
 
-  $(".close-button").click(function () {
-    $("#profile").fadeOut(1200);
-    $(".name").delay(1200).fadeIn(1200);
-    $(".profession").delay(1200).fadeIn(1200);
-    $(".arrow-left").delay(1200).fadeIn(1200);
-    $(".arrow-right").delay(1200).fadeIn(1200);
+  $(".close").click(function () {
+    $(".introduce-details").fadeOut(1200);
+    $(".introduce").delay(1200).fadeIn(1200);
   });
 });
