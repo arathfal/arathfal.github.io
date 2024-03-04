1f99888695abbe0dc8ff40c430f996b3abaf8901
Aradea Atfal Risdianto <aradea.atfal@shipper.id>
2024-03-04 12:47:53 +0700

    feat: move 404.html file to public

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
-import pluginRewriteAll from 'vite-plugin-rewrite-all';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), pluginRewriteAll()],
+  plugins: [vue()],
   optimizeDeps: {
     esbuildOptions: {
       target: 'es2020',
